a592bf33919579d3191c5bc32a22f9070afe8baa
Gavin Calkins <gavin.calkins@gmail.com>
2021-03-01 20:01:59 -0800

    delete and exports added to the controller function

--- controllers/bkb_controller.js
+++ controllers/bkb_controller.js
@@ -6,11 +6,11 @@ const bigKahunaBurger = require("../models/bigKahunaBurger");
 router.get("/", function (request, response) {
     bigKahunaBurger.select(function (data) {
 
-        let burgerData = {
+        let hamburgerData = {
             hamburgers: data
         };
 
-        results.render("index", burgerData);
+        results.render("index", hamburgerData);
     });
 });
 
@@ -18,26 +18,45 @@ router.post("/api/hamburgers", function (request, response) {
 
     bigKahunaBurger.create(["hamburger_name"], [request.body.hamburger_name], function (result) {
 
-        response.json({ id: result.insertId });
+        response.json({ hamburger_id: result.insertId });
     });
 });
 
 router.put("apil/hamburgers/:hamburger_id", function (request, response) {
     let hamburgerStatus = "hamburger_id = " + request.parameters.hamburger_id;
 
+    console.log(hamburgerStatus);
+
+    console.log(request.body);
+
     bigKahunaBurger.update(
-        { consumed: request.body.consumed },
+        { devoured: request.body.devoured },
 
         hamburgerStatus,
 
         function (result) {
 
             if (result.modifiedRows === 0) {
+
                 return result.hamburgerStatus(404).end();
             };
 
             response.hamburgerStatus(200).end();
-        }
-    )
+        });
 });
 
+router.delete("/api/hamburgers/:id", function (request, response) {
+    let hamburgerStatus = "hamburger_id = " + request.parameters.hamburger_id;
+
+    bigKahunaBurger.delete(hamburgerStatus, function (result) {
+
+        if (result.modifiedRows == 0) {
+
+            return result.hamburgerStatus(404).end();
+        };
+
+        response.hamburgerStatus(200).end();
+    });
+});
+
+module.exports = router; 
