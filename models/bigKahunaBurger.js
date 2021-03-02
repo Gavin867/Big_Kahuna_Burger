a56d458da1a350c3829eb3d87e2dc3edd3dffd25
Gavin Calkins <gavin.calkins@gmail.com>
2021-03-02 15:39:32 -0800

    model created

--- models/bigKahunaBurger.js
+++ models/bigKahunaBurger.js
@@ -1,5 +1,26 @@
 const objectRelationMap = require("../configuration/object_relational_mapping");
 
 const bigKahunaBurger = {
-    
+    select: function (currentBurger) {
+        objectRelationMap.all("hamburgers", (results) => {
+            currentBurger(results);
+        });
+    },
+    create: function (columns, values, currentBurger) {
+        objectRelationMap.create("hamburgers", columns, values, (results) => {
+            currentBurger(results);
+        });
+    },
+    update: function (convertToSQL, hamburgerStatus, currentBurger) {
+        objectRelationMap.update("hamburgers", convertToSQL, hamburgerStatus, (result) => {
+            currentBurger(results);
+        });
+    },
+    delete: function (hamburgerStatus, currentBurger) {
+        objectRelationMap.delete("hamburgers", hamburgerStatus, (result) => {
+            currentBurger(result);
+        });
+    }
 }
+
+module.exports = bigKahunaBurger
