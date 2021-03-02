73a27e8c0b86ca5e80ca9ecda31416574d89fa95
Gavin Calkins <gavin.calkins@gmail.com>
2021-03-01 16:11:19 -0800

    select and insert functions added to orm

--- config/object-relational-mapping.js
+++ config/object-relational-mapping.js
@@ -1,6 +1,6 @@
 const connection = require("./connection");
 
-function renderQuestionMarks(number) {
+function addQuestionMark(number) {
     let array = [];
 
     for (i = 0; i < number; i++) {
@@ -18,12 +18,42 @@ function convertToSQL (object) {
 
         let value = object[key];
 
-        if (Object.hasOwnProperty.call(oject, key)) {
+        if (Object.hasOwnProperty.call(object, key)) {
             if (typeof value === "string" && value.indexOf(" ") >= 0) {
                 value = "'" + value + "'";
             };
+            array.push(key + "=" + value);
         };
     };
 
     return array.toString();
+};
+
+const objectRelationalMapping = {
+
+    selectAll: function (tableInsert, currentBurger) {
+        let stringQuery = `SELECT * FROM ${tableInsert};`
+    
+        connection.query(stringQuery, values, function (error, data) {
+            if (error) {
+                throw error;
+            };
+
+            currentBurger(data);
+        });
+    },
+
+    insertBurger: function (table, columns, values, currentBurger) {
+        let stringQuery = `INSERT INTO ${table} (${columns.toString()}) VALUES (${addQuestionMark(values.length)})`
+    
+        console.log(stringQuery);
+
+        connection.query(stringQuery, values, function (error, data) {
+            if (error) {
+                throw error;
+            };
+
+            currentBurger(data);
+        });
+    }
 };
