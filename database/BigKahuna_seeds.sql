0c0b3359847fc5068e97782379eeabe1bba1a76a
Gavin Calkins <gavin.calkins@gmail.com>
2021-03-03 12:52:14 -0800

    bootstrap js added to main

---
+++ database/BigKahuna_seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO hamburgers (hamburger_name) VALUE ("Hamburger");
+INSERT INTO hamburgers (hamburger_name) VALUE ("Cheeseburger");
+INSERT INTO hamburgers (hamburger_name) VALUE ("Whiskey BBQ Burger");
+INSERT INTO hamburgers (hamburger_name) VALUE ("Mushroom Swiss Burger");
