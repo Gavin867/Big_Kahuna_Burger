21ac13f8881268b0e5e0e4cb00e5a4bb5316fd22
Gavin Calkins <gavin.calkins@gmail.com>
2021-03-03 20:04:40 -0800

    working on views and seed

--- database/BigKahuna_seeds.sql
+++ database/BigKahuna_seeds.sql
@@ -1,4 +1,8 @@
-INSERT INTO hamburgers (hamburger_name) VALUE ("Hamburger");
-INSERT INTO hamburgers (hamburger_name) VALUE ("Cheeseburger");
-INSERT INTO hamburgers (hamburger_name) VALUE ("Whiskey BBQ Burger");
-INSERT INTO hamburgers (hamburger_name) VALUE ("Mushroom Swiss Burger");
+USE bigkahuna_DB;
+INSERT INTO hamburgers (hamburger_name, hamburger_devoured)
+VALUES
+("Royale", FALSE),
+("Royale with Cheese", FALSE),
+("Le Big Mac", FALSE),
+("The Buddy Holly", TRUE),
+("Jack Rabbit Slim's Special", TRUE)
