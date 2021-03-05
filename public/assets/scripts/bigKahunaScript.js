2d7c1da7fa1723edf2befe5ccd24f5740b782ec9
Gavin Calkins <gavin.calkins@gmail.com>
2021-03-04 21:56:13 -0800

    post added to the javascript file

--- public/assets/scripts/bigKahunaScript.js
+++ public/assets/scripts/bigKahunaScript.js
@@ -1,37 +1,21 @@
-$(function() {
-    $(".devoured-state").on("click", function(e) {
-        let id = $(this).data("hamburger_id");
+$(function () {
+    $("#createBurger").on("click", function () {
+        const newBurger = $("#createBurger").val().trim();
+        const body = {
+            hamburger_name : newBurger,
+            hamburger_devoured: 0
+        }
 
-        let newBurger = $(this).data("newburger");
-
-        let burgerAte = {
-            hamburger_devoured: newBurger
-        };
-
-        $.ajax("/api/hamburgers/" + id, {
-            type: "PUT",
-            data: burgerAte
-        })
-        .then(function() {
+        if (newBurger === "") {
+            alert("Hamburgers must have a valid name");
+            return;
+        }
+        $.ajax({
+            url: "/api/hamburgers",
+            method: "POST",
+            data: body
+        }).then(function (result) {
             location.reload();
         });
     });
-
-    $(".create-form").on("submit", function(e) {
-
-        e.preventDefault();
-
-        let newBurger = {
-            hamburger_name: $("#hamburger_name").val().trim(),
-        };
-
-        $.ajax("/api/hamburgers", {
-            type: "POST",
-            data: newBurger
-        })
-        .then(function() {
-            location.reload();
-        });
-    });
-
 });
